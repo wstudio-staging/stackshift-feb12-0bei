fa801e9c67640ae9b777d7f45a3e74d7d3383ee4
wstudio-staging <98862409+wstudio-staging@users.noreply.github.com>
2024-02-12 09:10:33 +0800

    Initialize john/seo-default

---
+++ helper/index.tsx
@@ -0,0 +1,89 @@
+import Link from "next/link";
+import { Logo } from "types";
+
+// WebriQ form redirect thank you page on successful submission
+export const thankYouPageLink = (link) => {
+  if (!link) {
+    return "/thank-you";
+  } else {
+    if (link?.linkType === "linkInternal") {
+      return `/${link?.internalLink}`;
+    } else {
+      return link?.externalLink;
+    }
+  }
+};
+
+// Logo link
+export const logoLink = (logo: Logo) => {
+  if (logo?.internalLink && logo?.type === "linkInternal") {
+    if (logo?.internalLink?.toLowerCase()?.includes("home")) {
+      return "/";
+    }
+    return `/${logo.internalLink}`;
+  } else if (logo?.externalLink && logo?.type === "linkExternal") {
+    return logo?.externalLink ?? "/";
+  } else {
+    return "/";
+  }
+};
+
+// Conditional links
+export const ConditionalBtnOrLink = ({ value, style }) => {
+  const defaultStyle =
+    "inline-block py-2 px-6 rounded-l-xl rounded-t-xl bg-webriq-darkblue hover:bg-webriq-blue text-gray-50 font-bold leading-loose outline-none transition duration-200";
+
+  if (
+    (value?.internalLink || value?.linkInternal) &&
+    value?.type === "linkInternal"
+  ) {
+    // internal link
+    if (value?.internalLink?.toLowerCase() === "home") {
+      return (
+        <Link
+          aria-label={value?.label}
+          className={style ?? defaultStyle}
+          target={value?.linkTarget}
+          rel={value?.linkTarget === "_blank" ? "noopener noreferrer" : null}
+          href={`/`}
+        >
+          {value?.label}
+        </Link>
+      );
+    } else {
+      return (
+        <Link
+          aria-label={value?.label}
+          className={style ?? defaultStyle}
+          target={value?.linkTarget}
+          rel={value?.linkTarget === "_blank" ? "noopener noreferrer" : null}
+          href={`/${value.internalLink}`}
+        >
+          {value?.label}
+        </Link>
+      );
+    }
+  } else if (
+    (value?.externalLink || value?.linkExternal) &&
+    value?.type === "linkExternal"
+  ) {
+    // external link (includes mailto and tel)
+    return (
+      <Link
+        aria-label={value?.label}
+        className={style ?? defaultStyle}
+        target={value?.linkTarget}
+        href={value?.externalLink}
+        rel={value?.linkTarget === "_blank" ? "noopener noreferrer" : null}
+      >
+        {value?.label}
+      </Link>
+    );
+  } else {
+    return (
+      <a className={style ?? defaultStyle} href="/link-not-found">
+        {value?.label}
+      </a>
+    );
+  }
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,61 @@
+import "../styles/globals.css";
+import React, { useEffect } from "react";
+import SEO from "../components/SEO";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import useScript from "utils/useScript";
+import { useRouter } from "next/router";
+
+function MyApp({ Component, pageProps }) {
+  let script_status = useScript(process.env.NEXT_PUBLIC_ECWID_SCRIPT);
+  const { preview } = pageProps;
+  const router = useRouter();
+
+  // patch: cleanup `secret=<secret>&slug=<slug>` when on preview mode as this causes ECWID to refresh indefinitely
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      preview &&
+      location.search?.includes("secret=") &&
+      location.search?.includes("slug=")
+    ) {
+      router.push(`${window.location.pathname}`);
+    }
+  }, [preview, router]);
+
+  // for Ecwid script
+  useEffect(() => {
+    if (script_status === "ready") {
+      try {
+        window.Ecwid.OnAPILoaded.add(function () {
+          window.Ecwid.init();
+        });
+        window.Ecwid.OnPageLoaded.add(function (page) {
+          if (page.type === "CATEGORY" || page.type === "PRODUCT") {
+            Ecwid.openPage("cart");
+          }
+
+          if (page.type === "CART") {
+            let elem = document.querySelector(".ec-cart--empty button");
+            elem.addEventListener("click", (e) => {
+              e.preventDefault();
+              window.location.href = "/collections/all-products";
+            });
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [script_status]);
+
+  return (
+    <>
+      <SEO {...pageProps} />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default React.memo(MyApp);
